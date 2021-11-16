15c9232cbcd81235a2cc88a2292956c04bd4daf7
Samson Hailu <alxsamsonhailu@gmail.com>
2021-11-16 20:15:37 +0300

    Print alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-
-
-def print_low_alpha_ex():
-    for n in range(97, 123):
-        if n != 101 and n != 113:
-            print('{}'.format(chr(n)), end='')
-
-
-if __name__ == '__main__':
-    print_low_alpha_ex()
+for c in range(97, 123):
+    if c != 101 and c != 113:  # not 'e' and 'q'
+        print("{:c}".format(c), end="")
