15c72d7cdd6a32810cf243e4b5a8d2e17fb7756a
Samson Hailu <alxsamsonhailu@gmail.com>
2021-11-19 23:19:15 +0300

    Args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    import sys
-    argc = len(sys.argv) - 1
-    str = "argument"
-    if argc == 0:
-        print("0 {}s.".format(str))
-    else:
-        if argc > 1:
-            str += "s"
-        str += ":"
-        print(argc, str)
 
-        for i in range(1, argc + 1):
-            print("{}:".format(i), sys.argv[i])
+if __name__ == '__main__':
+    from sys import argv
+
+    if len(argv) == 1:
+        print('0 arguments.')
+    elif len(argv) == 2:
+        print('{:d} argument:'.format(len(argv) - 1))
+    else:
+        print('{:d} arguments:'.format(len(argv) - 1))
+
+    for i, arg in enumerate(argv[1:], start=1):
+        print('{:d}: {:s}'.format(i, arg))
