f357cda6fb928d4ee3809e8b54c8a7616247627d
Samson Hailu <alxsamsonhailu@gmail.com>
2021-12-07 20:30:21 +0300

    Update Simple

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Simple Square class"""
+
+
+class Square:
+    """Simple class with attrs validation.
+    Args:
+        size (int): Size of the square
+    """
+    def __init__(self, size=0):
+        self.size = size
+
+    def area(self):
+        """Method to calculate the area of the square"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """size: property to modify private __size attr"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+    def my_print(self):
+        """This function prints the square using '#' chars"""
+        if self.__size == 0:
+            print()
+            return
+
+        rang = range(self.__size)
+        for _ in rang:
+            print('#' * self.__size)
