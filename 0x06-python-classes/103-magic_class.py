4dac2738510cbc4a39c760e3a5f50bc948c0b3c0
Samson Hailu <alxsamsonhailu@gmail.com>
2021-12-07 20:53:10 +0300

    Magic class

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""MagiClass declaration"""
+
+
+class MagicClass:
+    """Simple class with attrs validation"""
+
+    def __init__(self, radius=0):
+        """Instance inicialization
+        Args:
+            radius (int/float): Radius attr
+        """
+
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+
+        self.__radius = radius
+
+    def area(self):
+        """area: Calculates the area"""
+        return (self.__radius ** 2) * __import__('math').pi
+
+    def circumference(self):
+        """circumference: Calculates the circumference"""
+        return 2 * __import__('math').pi * self.__radius
