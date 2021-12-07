228ee9c045a335545a92481de59f47cb4f657a19
Samson Hailu <alxsamsonhailu@gmail.com>
2021-12-07 20:23:53 +0300

    Class with attributes

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Class with attributes validation"""
+
+
+class Square:
+    """Simple class with attrs validation.
+    Args:
+        size (int): Size of the square
+    Raises:
+        TypeError: If size type is not int
+        ValueError: If size < 0
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
